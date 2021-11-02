dac7e33fb58dba4591f1b7e76ebaa33f2ae10350
Ayfri <pierre.ayfri@gmail.com>
2021-11-02 20:05:50 +0100

    refactor: Simplify Main tests.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,52 @@
 
-import kotlinext.js.get
+import kotlinext.js.Record
+import kotlinext.js.set
 import kotlinx.browser.document
 import kotlinx.browser.window
+import org.w3c.dom.Document
 import typings.app.Application
-import typings.core.Texture
-import typings.loaders.Loader
 import typings.sprite.Sprite
 import typings.ticker.Ticker
-import typings.ticker.UPDATE_PRIORITY
+import utils.create
 import kotlin.random.Random
 
+var data: Record<Any, Any> = Record {}
+
 fun main() {
-	val app = Application()
-	app.resizeTo = window
-	val sprite = Sprite.from(Texture.WHITE).apply {
+	val app = Application.create {
+		width = window.innerWidth
+		height = window.innerHeight
+		backgroundColor = 08080808f
+	}
+	
+	document.getElementById("root")!!.appendChild(app.view)
+	
+	val sprite = Sprite.from("test.png").apply {
 		width = 100
 		height = 100
 		x = window.innerWidth / 2
 		y = window.innerHeight / 2
 	}
 	
-	app.stage.addChild(sprite)
-	
-	window.onload = {
-		val ticker = Ticker.shared.add<Any>({ _, _ ->
-			sprite.apply {
-				x = Random.nextFloat() * window.innerWidth
-				y = Random.nextFloat() * window.innerHeight
-			}
-		}, {}, UPDATE_PRIORITY.HIGH)
-		
-		console.log(ticker)
-		
-		Loader.shared.add("test", "./test.png")
-		Loader.shared.load { _, resources ->
-			sprite.texture = resources["test"]!!.texture!!
+	val ticker = Ticker.shared.add<Any>({ _, _ ->
+		sprite.apply {
+			x = Random.nextFloat() * window.innerWidth
+			y = Random.nextFloat() * window.innerHeight
 		}
-		
-		document.getElementById("root")!!.appendChild(app.view)
-	}
+	})
+
+	console.log(ticker)
+	app.stage.addChild(sprite)
+	console.log(app);
+	
+//	document.body!!.appendChild(app.view)
+	
+//		val canvas =
+	document.d()["body"] = document.body!!
+	console.log(document.d())
+	
+	
 }
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun Document.d(): Record<Any, Any> = data
