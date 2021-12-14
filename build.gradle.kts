6dc90143ed47e3f9958df7391ec3717c1fdbbef4
Ayfri <pierre.ayfri@gmail.com>
2021-12-14 22:37:05 +0100

    fix: Remove not existing tasks.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,62 +8,60 @@ group = Project.group
 version = Project.version
 
 repositories {
-    mavenCentral()
-    Dependencies.mavenRepositories.forEach { r -> maven { url = uri(r) }}
+	mavenCentral()
+	Dependencies.mavenRepositories.forEach { r -> maven { url = uri(r) }}
 }
 
 plugins {
-    Dependencies.plugins.forEach { (n, v) -> id(n) version v }
+	Dependencies.plugins.forEach { (n, v) -> id(n) version v }
 }
 
 dependencies {
-    Dependencies.dependencies.forEach { (n, v) -> implementation("$n:$v") }
-    Dependencies.npmDependencies.forEach { (n, v) -> implementation(npm(n, v)) }
+	Dependencies.dependencies.forEach { (n, v) -> implementation("$n:$v") }
+	Dependencies.npmDependencies.forEach { (n, v) -> implementation(npm(n, v)) }
 }
 
 tasks {
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = Versions.java
-    }
-    
-    create("buildAndPublish") {
-        group = "publishing"
-        description = "Builds and publishes the project"
-        dependsOn("clean")
-        dependsOn("build")
-        dependsOn("preparePublish")
-        dependsOn("publishAllPublicationsToSonatypeRepository", "closeAndReleaseSonatypeStagingRepository")
-        dependsOn("prepareDevelopment")
-    }
+	withType<KotlinCompile> {
+		kotlinOptions.jvmTarget = Versions.java
+	}
+	
+	create("buildAndPublish") {
+		group = "publishing"
+		description = "Builds and publishes the project"
+		dependsOn("clean")
+		dependsOn("build")
+		dependsOn("publishAllPublicationsToSonatypeRepository", "closeAndReleaseSonatypeStagingRepository")
+	}
 }
 
 fun getExtraString(name: String) = ext[name]?.toString()
 
 nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-        }
-    }
+	repositories {
+		sonatype {
+			nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+		}
+	}
 }
 
 rootProject.plugins.withType<YarnPlugin> {
-    rootProject.the<YarnRootExtension>().apply {
-        resolution("@webpack-cli/serve", "1.5.2")
-    }
+	rootProject.the<YarnRootExtension>().apply {
+		resolution("@webpack-cli/serve", "1.5.2")
+	}
 }
 
 kotlin {
-    kotlinDaemonJvmArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
-
-    js(IR) {
-        useCommonJs()
-        browser {
-            commonWebpackConfig {
-                cssSupport.enabled = true
-                outputFileName = "dist.js"
-            }
-        }
-        binaries.executable()
-    }
+	kotlinDaemonJvmArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+	
+	js(IR) {
+		useCommonJs()
+		browser {
+			commonWebpackConfig {
+				cssSupport.enabled = true
+				outputFileName = "dist.js"
+			}
+		}
+		binaries.executable()
+	}
 }
