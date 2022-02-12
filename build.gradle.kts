b165a20e408cdae9bd7c48d7bfd646896f9897aa
Ayfri <pierre.ayfri@gmail.com>
2022-02-12 21:09:05 +0100

    fix: Fix url.

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,7 +105,8 @@ publishing {
 nexusPublishing {
 	repositories {
 		sonatype {
-			nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
+			nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+			snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
 			username.set(System.getenv("OSSRH_USER") ?: return@sonatype)
 			password.set(System.getenv("OSSRH_PASSWORD") ?: return@sonatype)
 		}
