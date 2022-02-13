350fedf84d28f36c9fb3897b08a8818c5b58ab63
Ayfri <pierre.ayfri@gmail.com>
2022-02-13 03:17:27 +0100

    feat: Update version to 0.1.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 object Project {
 	const val group = "io.github.ayfri"
-	const val version = "0.1.0"
+	const val version = "0.1.1"
 	const val name = "PIXI-Kotlin"
 	const val description = "Kotlin bindings for PIXI.js"
 	const val githubUrl = "Ayfri/PIXI-kotlin"
