640d21da98746d8558ba4b39623aedeec87bc83a
Ayfri <pierre.ayfri@gmail.com>
2022-02-26 16:40:22 +0100

    chore: Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ repositories {
 dependencies {
 	implementation("io.github.turansky.seskar:seskar-core:0.5.0")
 	implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.3")
-	implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions:1.0.1-pre.299-kotlin-1.6.10")
+	implementation("org.jetbrains.kotlin-wrappers:kotlin-extensions:1.0.1-pre.307-kotlin-1.6.10")
 	implementation(npm("pixi.js", "6.2.2"))
 	implementation(npm("@pixi/events", "6.2.2"))
 	testImplementation(kotlin("test-js"))
