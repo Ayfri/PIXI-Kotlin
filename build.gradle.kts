ee6c5606b00450d26d4c841c813836f3762f7d3d
Ayfri <pierre.ayfri@gmail.com>
2022-04-21 16:46:47 +0200

    chore: Add publishAll task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,25 +2,37 @@ import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
 plugins {
-	id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+	id("io.github.gradle-nexus.publish-plugin")
 	kotlin("js") apply false
 }
 
-allprojects {
-	nexusPublishing {
-		repositories {
-			sonatype {
-				nexusUrl.set(uri(Project.publishUrl))
-				snapshotRepositoryUrl.set(uri(Project.snapshotUrl))
-				username.set(System.getenv("OSSRH_USER") ?: return@sonatype)
-				password.set(System.getenv("OSSRH_PASSWORD") ?: return@sonatype)
-			}
+nexusPublishing {
+	repositories {
+		sonatype {
+			nexusUrl.set(uri(Project.publishUrl))
+			snapshotRepositoryUrl.set(uri(Project.snapshotUrl))
+			username.set(System.getenv("OSSRH_USER") ?: return@sonatype)
+			password.set(System.getenv("OSSRH_PASSWORD") ?: return@sonatype)
 		}
+	}
+}
+
+val initializeSonatypeStagingRepository by tasks.existing
+subprojects {
+	initializeSonatypeStagingRepository {
+		shouldRunAfter(tasks.withType<Sign>())
 	}
 }
 
 tasks.wrapper {
 	gradleVersion = "7.4.2"
+}
+
+tasks.register("publishAll") {
+	group = "publishing"
+	description = "Publish all submodules at once."
+	
+	dependsOn(subprojects.map { ":${it.path}:buildAndPublish" })
 }
 
 plugins.withType<YarnPlugin> {
