28b1e1708097f6f0bfec911c4e220890587c0aca
Ayfri <pierre.ayfri@gmail.com>
2022-06-29 22:09:25 +0200

    chore: Update version to 0.5.1.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
 
 object Project {
 	const val group = "io.github.ayfri"
-	const val version = "0.5.0"
+	const val version = "0.5.1"
 	const val name = "PIXI-Kotlin"
 	const val description = "Kotlin bindings for PIXI.js"
 	const val githubUrl = "Ayfri/PIXI-kotlin"
